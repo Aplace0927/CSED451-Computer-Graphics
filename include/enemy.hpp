1afdfc8b0b0850ee574eae37f3987e4bcf1d2662
Aplace <aplace030927@gmail.com>
2025-09-16 00:15:01 +0900

    feat(enemy): add basic trait of enemy

--- include/enemy.hpp
+++ include/enemy.hpp
@@ -1,4 +1,15 @@
 #ifndef ENEMY_HPP
 #define ENEMY_HPP
 
+#include "object.hpp"
+
+namespace Enemy {
+    class Enemy : public Object::Object<glm::vec3, Shape::RGBColor> {
+    public:
+        Enemy();
+    private:
+        // Add enemy state variables here 
+        int enemyHealth;
+    };
+};
 #endif // ENEMY_HPP
