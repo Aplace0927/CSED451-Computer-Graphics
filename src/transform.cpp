e1178956fc71783645c2ccc41abeec1d01ea4eba
Aplace <aplace030927@gmail.com>
2025-10-10 16:20:46 +0900

    chore(transformation): eye-candied

--- src/transform.cpp
+++ src/transform.cpp
@@ -1,4 +1,5 @@
 #include "transform.hpp"
+#include "healthbar.hpp"
 
 namespace Transform {
     Transform<glm::vec3, Shape::RGBColor>* createPlayer() {
@@ -29,7 +30,17 @@ namespace Transform {
             float angle = glm::radians(i * 5.0f); // 5
             orbitPoints.push_back(glm::vec3(30.0f * glm::cos(angle), 30.0f * glm::sin(angle), 0.0f));
         }
-        std::vector<Shape::RGBColor> orbitColors(72, Shape::RGBColor(1.0f, 0.0f, 1.0f));
+        std::vector<Shape::RGBColor> orbitColors;
+        for (int i = 0; i < 72; i++) {
+            float v;
+            if (i > 36) {
+                v = (72 - i) / 36.0f;
+            }
+            else {
+                v = i / 36.0f;
+            }
+            orbitColors.push_back(Shape::RGBColor(0.0f, v, 1.0f - v));
+        }
         Shape::Shape<glm::vec3, Shape::RGBColor> playerHealthOrbit(
             std::vector<std::vector<glm::vec3>>{orbitPoints},
             std::vector<std::vector<Shape::RGBColor>>{orbitColors},
@@ -48,17 +59,23 @@ namespace Transform {
                 glm::vec3(0.0f, 0.0f, 1.0f)
             );
         };
-        
-        
+
+        const float gemSize = 3.0f;
         Shape::Shape<glm::vec3, Shape::RGBColor> playerHealthGem(
             std::vector<std::vector<glm::vec3>> {{
-                    glm::vec3(-5.0f, 5.0f, 0.0f),
-                    glm::vec3( 5.0f, 5.0f, 0.0f),
-                    glm::vec3( 5.0f,-5.0f, 0.0f),
-                    glm::vec3(-5.0f,-5.0f, 0.0f),
-                    glm::vec3(-5.0f, 5.0f, 0.0f)
+                    glm::vec3(-gemSize, gemSize, 0.0f),
+                    glm::vec3( gemSize, gemSize, 0.0f),
+                    glm::vec3( gemSize,-gemSize, 0.0f),
+                    glm::vec3(-gemSize,-gemSize, 0.0f),
+                    glm::vec3(-gemSize, gemSize, 0.0f)
             }},
-            std::vector<std::vector<Shape::RGBColor>>{std::vector<Shape::RGBColor>(5, Shape::RGBColor(1.0f, 0.0f, 1.0f))},
+            std::vector<std::vector<Shape::RGBColor>>{{
+                Shape::RGBColor(0.0f, 0.0f, 1.0f),
+                Shape::RGBColor(0.0f, 0.5f, 0.5f),
+                Shape::RGBColor(0.0f, 1.0f, 0.0f),
+                Shape::RGBColor(0.0f, 0.5f, 0.5f),
+                Shape::RGBColor(0.0f, 0.0f, 1.0f)
+            }},
             std::vector<unsigned int>{GL_TRIANGLE_FAN}
         );
 
@@ -112,7 +129,23 @@ namespace Transform {
         
         enemyTransform->transformAnimateMatrix = [](time_t time) { return glm::identity<glm::mat4>(); };
 
-        const float legLength = 20.0f;
+        Transform<glm::vec3, Shape::RGBColor>* healthBarTransform = new Transform<glm::vec3, Shape::RGBColor>(
+            HealthBar::HealthBar(
+                glm::vec3(-30.0f, 60.0f, 0.0f), // digit position
+                10.0f, // digit size
+                glm::vec3(-40.0f, 40.0f, 0.0f), // gauge position
+                80.0f, // gauge width
+                10.0f, // gauge height
+                GameConfig::ENEMY_LIFE // max health
+            )
+        );
+        healthBarTransform->hasBoundingBox = false; // Health bar does not contribute to bounding box
+        healthBarTransform->transformMatrix = glm::identity<glm::mat4>();
+        healthBarTransform->transformAnimateMatrix = [](time_t time) { return glm::identity<glm::mat4>(); };
+        enemyTransform->addChild(healthBarTransform);
+
+        const float legLength = 25.0f;
+        const float legSwingAngle = 30.0f;
         Shape::Shape<glm::vec3, Shape::RGBColor> enemyLeg(
             std::vector<std::vector<glm::vec3>>{{
                 glm::vec3(0.0f, -5.0f, 0.0f),
@@ -122,44 +155,73 @@ namespace Transform {
                 glm::vec3(0.0f, -5.0f, 0.0f),
             }},
             std::vector<std::vector<Shape::RGBColor>> {{
-                Shape::RGBColor(0.0f, 0.0f, 1.0f),
-                Shape::RGBColor(0.0f, 0.0f, 1.0f),
-                Shape::RGBColor(0.0f, 0.0f, 1.0f),
-                Shape::RGBColor(0.0f, 0.0f, 1.0f),
-                Shape::RGBColor(0.0f, 0.0f, 1.0f),
+                Shape::RGBColor(1.0f, 0.0f, 0.0f),
+                Shape::RGBColor(0.3f, 0.0f, 0.0f),
+                Shape::RGBColor(0.3f, 0.0f, 0.0f),
+                Shape::RGBColor(1.0f, 0.0f, 0.0f),
+                Shape::RGBColor(1.0f, 0.0f, 0.0f),
             }},
             std::vector<unsigned int>{GL_TRIANGLE_FAN}
         );
 
-        Transform<glm::vec3, Shape::RGBColor>* enemyLegTransform_1 = new Transform<glm::vec3, Shape::RGBColor>(enemyLeg);
-        enemyLegTransform_1->transformMatrix = glm::translate(
-            glm::identity<glm::mat4>(),
-            glm::vec3(legLength * 2, 0.0f, 0.2f)
-        );
-        enemyLegTransform_1->transformAnimateMatrix = [](time_t time) {
-            return glm::rotate(
+        double legAngle[4] = {30.0, 150.0, 210.0, 330.0};
+        for (double d: legAngle) {
+            Transform<glm::vec3, Shape::RGBColor>* enemyLegTransformUpper = new Transform<glm::vec3, Shape::RGBColor>(enemyLeg);
+            enemyLegTransformUpper->transformMatrix = glm::translate(
                 glm::identity<glm::mat4>(),
-                glm::radians(Utility::getPhaseOscillator<float>(20.0f, -20.0f, 0.2f)),
-                glm::vec3(0.0f, 0.0f, 1.0f)
+                glm::vec3(
+                    cos(glm::radians(d)) * legLength,
+                    sin(glm::radians(d)) * legLength,
+                    0.2f
+                )
             );
-        };
 
-        Transform<glm::vec3, Shape::RGBColor>* enemyLegTransform_2 = new Transform<glm::vec3, Shape::RGBColor>(enemyLeg);
-        enemyLegTransform_2->transformMatrix = glm::translate(
-            glm::identity<glm::mat4>(),
-            glm::vec3(legLength, 0.0f, 0.2f)
-        );
-        enemyLegTransform_2->transformAnimateMatrix = [](time_t time) {
-            return glm::rotate(
+            Transform<glm::vec3, Shape::RGBColor>* enemyLegTransformLower = new Transform<glm::vec3, Shape::RGBColor>(enemyLeg);
+            enemyLegTransformLower->transformMatrix = glm::translate(
                 glm::identity<glm::mat4>(),
-                glm::radians(Utility::getPhaseOscillator<float>(20.0f, -20.0f, 0.3f)),
-                glm::vec3(0.0f, 0.0f, 1.0f)
+                glm::vec3(
+                    legLength,
+                    0.0f,
+                    0.2f
+                )
             );
-        };
 
-        
-        enemyTransform->addChild(enemyLegTransform_1);
-        enemyLegTransform_1->addChild(enemyLegTransform_2);
+            if (90.0f < d && d < 270.0f) {
+                enemyLegTransformUpper->transformAnimateMatrix = [legSwingAngle](time_t time) {
+                    return glm::rotate(
+                        glm::identity<glm::mat4>(),
+                        glm::radians(180.0f - Utility::getPhaseOscillator<float>(-legSwingAngle, legSwingAngle, 0.2f)),
+                        glm::vec3(0.0f, 0.0f, 1.0f)
+                    );
+                };
+                enemyLegTransformLower->transformAnimateMatrix = [legSwingAngle](time_t time) {
+                    return glm::rotate(
+                        glm::identity<glm::mat4>(),
+                        glm::radians(180.0f - Utility::getPhaseOscillator<float>(-legSwingAngle, legSwingAngle, 0.3f)),
+                        glm::vec3(0.0f, 0.0f, 1.0f)
+                    );
+                };
+            }
+            else {
+                enemyLegTransformUpper->transformAnimateMatrix = [legSwingAngle](time_t time) {
+                return glm::rotate(
+                    glm::identity<glm::mat4>(),
+                    glm::radians(Utility::getPhaseOscillator<float>(-legSwingAngle, legSwingAngle, 0.2f)),
+                    glm::vec3(0.0f, 0.0f, 1.0f)
+                );
+            };
+                enemyLegTransformLower->transformAnimateMatrix = [legSwingAngle](time_t time) {
+                    return glm::rotate(
+                        glm::identity<glm::mat4>(),
+                        glm::radians(-180.0f + Utility::getPhaseOscillator<float>(-legSwingAngle, legSwingAngle, 0.3f)),
+                        glm::vec3(0.0f, 0.0f, 1.0f)
+                    );
+                };
+            }
+
+            enemyTransform->addChild(enemyLegTransformUpper);
+            enemyLegTransformUpper->addChild(enemyLegTransformLower);
+        }
         return enemyTransform;
     }
 
