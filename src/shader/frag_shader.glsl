2422d02836fe720bbe1c2558afa1f5df536a69df
Aplace <aplace030927@gmail.com>
2025-11-28 19:00:58 +0900

    feat(shader): add texture mapping on shader

--- src/shader/frag_shader.glsl
+++ src/shader/frag_shader.glsl
@@ -1,9 +1,15 @@
 #version 330 core
 
-uniform vec3 uColor;
+in vec3 inFragVec3Position;
+in vec3 inFragVec3Normal;
+in vec2 outVec2TexCoord;
 
+uniform sampler2D samp2DTexture;
+
+uniform vec3 uColor;
+uniform float uFloatUseTexture;
 out vec4 FragColor;
 
 void main() {
-    FragColor = vec4(uColor, 1.0);
+    FragColor = mix(texture(samp2DTexture, outVec2TexCoord), vec4(uColor, 1.0), 1.0 - uFloatUseTexture);
 }
--- src/shader/vert_shader.glsl
+++ src/shader/vert_shader.glsl
@@ -10,10 +10,12 @@ uniform mat4 uMat4Model;
 uniform mat4 uMat4CameraShake;
 
 out vec3 outVec3WorldPosition;
+out vec2 outVec2TexCoord;
 
 void main() {
     vec4 worldPos = uMat4Model * vec4(inVec3Position, 1.0);
     outVec3WorldPosition = worldPos.xyz;
-
+    outVec2TexCoord = inVec2TexCoord;
+    
     gl_Position = uMat4Projection * uMat4CameraShake * uMat4View * worldPos;
 }
